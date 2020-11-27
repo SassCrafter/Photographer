bb9fb89fda1c15d35e0385cb8fa7b5407aca2918
Dima <dima@gmail.com>
2020-11-27 16:43:54 +0200

    Changed nav and finished website

--- dist/js/app.js
+++ dist/js/app.js
@@ -21,3 +21,39 @@ window.addEventListener('scroll', function() {
     else { remove_class_on_scroll() }
 
 });
+
+
+// Change nav overlay img on hover
+const navLinks = document.querySelectorAll('.nav a');
+const navOverlay = document.querySelectorAll('.nav-overlay');
+const active1 = document.querySelector('.active-1');
+const linkActive = document.querySelector('.link-active');
+
+navLinks.forEach(el => {
+    el.addEventListener('mouseover', function() {
+        let linkData = el.getAttribute('data-overlay');
+        navLinks.forEach(el => {
+            el.classList.remove('active');
+        })
+        el.classList.add('active');
+        navOverlay.forEach(el => {
+            if (el.getAttribute('data-overlay') == linkData) {
+                navOverlay.forEach(el => {
+                    el.classList.remove('active');
+                })
+                el.classList.add('active');
+            }
+        })
+    });
+
+    el.addEventListener('mouseleave', function() {
+        navLinks.forEach(el => {
+            el.classList.remove('active')
+        });
+        linkActive.classList.add('active');
+        navOverlay.forEach(el => {
+            el.classList.remove('active');
+        });
+        active1.classList.add('active');
+    })
+})
